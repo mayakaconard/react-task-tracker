db6f7e33e27ffee2c87641eddff73f76874bf883
mayakaconard <mayakaconard@gmail.com>
2021-04-02 03:17:23 +0300

    Handling Events

--- src/App.js
+++ src/App.js
@@ -19,11 +19,17 @@ const App = () =>
       "reminder": true
     }
   ])
+
+  // Delete Task
+  const deleteTask = (id) =>
+  {
+    SetTasks(tasks.filter((task) => task.id !== id))
+  }
   return (
     <div className="container">
 
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No task found!'}
     </div>
   );
 }
